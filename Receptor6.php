beee3b4f92b89b7aad620f4f2ef496d410703dd5
AndyRivera1605 <andyrivera16016@gmail.com>
2026-05-20 11:25:34 -0300

    Ejercicio_6
    Elabore un formulario que solicite el nombre y la edad de una persona y envíe la
    información a otro archivo PHP donde se muestren ambos datos.

---
+++ Receptor6.php
@@ -0,0 +1,14 @@
+<?php
+$Nombre = $_POST ['nombre'];
+$Edad = $_POST ['age'];
+
+echo "<center>";
+
+echo "<h1> Datos Ingresados : </h1>";
+echo "<br><br>";
+echo "Su nombre Completo es; " . $Nombre;
+echo "<br><br>";
+echo "La edad Ingresada es: " . $Edad;
+
+echo "</center>"
+?>
