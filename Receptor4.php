2f5edca8b65ada38c829c9f62e07eadb3d692c2f
AndyRivera1605 <andyrivera16016@gmail.com>
2026-05-20 11:19:49 -0300

    Ejercicio 4
    Cree un formulario que solicite dos números y envíe los datos a otro archivo PHP
    donde se calcule y muestre la multiplicación.

---
+++ Receptor4.php
@@ -0,0 +1,9 @@
+<?php
+$numero1 = $_POST ['numero5'];
+$numero2 = $_POST ['numero6'];
+
+$Multiplicacion = $numero1 * $numero2;
+
+echo "<h1> Binevenido al resultado de tu Multiplicacion: <br></h1>";
+echo "El resultado de tu Multiplicacion es: " . $Multiplicacion;
+?>
